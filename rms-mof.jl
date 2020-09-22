c066ffad14cb0b3334ea4acfbe88f27c35dccaad
CorySimon <CorySimon@berkeley.edu>
2020-09-21 21:44:32 -0700

    checked formulas with symbolic expressions

--- rms-mof.jl
+++ rms-mof.jl
@@ -245,6 +245,155 @@ begin
 end
 
 # ╔═╡ c5d406d2-fc8a-11ea-378d-7388e9ad53d4
+md"
+# RMS-MOF analysis
+"
+
+# ╔═╡ dfdda3b0-fc8a-11ea-3777-c7fc194bd218
+begin
+	struct Material
+	    δ::Float64
+	    ϵ♡::Float64
+	    ϵ△::Float64
+	end
+	
+	function Base.show(io::IO, material::Material)
+	    @printf("\tδ = %.3f\n\tϵ♡ = %.3f\n\tϵ△ = %.3f\n", 
+			    material.δ, material.ϵ♡, material.ϵ△)
+	end
+end
+
+# ╔═╡ fa8835e0-fc8a-11ea-00cf-25aa4e5c3ffb
+begin
+	# kT::Real as opposed to kT::Float64 for ForwardDiff.jl to work
+	
+	function K(material::Material, kT::Real) 
+	    p♡_n0 = 1 / (1 + exp(-material.δ / kT))
+	    return p♡_n0 * exp(-material.ϵ△ / kT) + (1 - p♡_n0) * exp(-material.ϵ♡ / kT)
+	end
+	
+	function n(material::Material, kT::Real, P::Float64)
+	    KβP = K(material, kT) * P / kT
+	    return KβP / (1.0 + KβP)
+	end
+	
+	function w(material::Material, kT::Real, P::Float64)
+		pΔ_n0 = exp(-material.δ / kT) / (1 + exp(-material.δ / kT))
+		K♡βP = exp(-material.ϵ♡ / kT) * P / kT
+		KβP = K(material, kT) * P / kT
+	    return pΔ_n0  * (1 + K♡βP) / (1 + KβP)
+	end
+	
+	function ∂E_∂n(material::Material, kT::Float64)
+	    β = 1 / (kT)
+	    w_n0 = exp(-β * material.δ) / (1 + exp(-β * material.δ))
+	    w_n1 = exp(-β * (material.ϵ♡ + material.δ)) / (exp(-β * material.ϵ△) + exp(-β * (material.ϵ♡ + material.δ)))
+	    return material.δ * (w_n1 - w_n0) + material.ϵ♡ * w_n1 + material.ϵ△ * (1 - w_n1)
+	end
+	
+	function ∂E□_∂n□(ϵ□::Float64)
+	    return ϵ□
+	end
+	
+	@assert ∂E□_∂n□(-4.0) ≈ ∂E_∂n(Material(0.0, -4.0, -4.0), rand())
+	@assert ∂E□_∂n□(-4.0) ≈ ∂E_∂n(Material(2.0, -4.0, -4.0), rand())
+	@assert ∂E□_∂n□(-2.0) ≈ ∂E_∂n(Material(2.0, -4.0, 0.0), rand()) # weird case
+end
+
+# ╔═╡ 409daf48-fc8c-11ea-0811-d3bbaa86a15b
+md"
+use `ForwardDiff.jl` to autodiff
+"
+
+# ╔═╡ 5b5c269a-fc8c-11ea-3eee-c3767d47d678
+begin
+	function ∂n_∂kT(material::Material, kT::Float64, P::Float64)
+		# view n as a function of kT only.
+		n_of_kT(x) = n(material, x[1], P) # x = [kT]
+		∂n_∂kT_of_T = x -> ForwardDiff.gradient(n_of_kT, x) # x plays role of kT
+		return ∂n_∂kT_of_T([kT])[1]
+	end
+	
+	function ∂K_∂kT(material::Material, kT::Float64)
+		# view K as a function of kT only.
+		K_of_kT(x) = K(material, x[1]) # x = [kT]
+		∂K_∂kT_of_kT = x -> ForwardDiff.gradient(K_of_kT, x) # x plays role of kT
+		return ∂K_∂kT_of_kT([kT])[1]
+	end
+end
+
+# ╔═╡ 8f1e2f58-fc8c-11ea-1fde-9d1d72b5d631
+md"
+## check these with symbolic differentiation in `SymPy`
+"
+
+# ╔═╡ 9a054b92-fc8c-11ea-0aaf-f90ed61e9586
+begin
+	# define symbol versions of the variables
+	_kT = Sym("kT")
+	_ϵ♡ = Sym("ϵ♡")
+	_ϵ△ = Sym("ϵ△")
+	_δ = Sym("δ")
+	_P = Sym("P")
+end
+
+# ╔═╡ 323ced64-fc8d-11ea-0ffe-7d5a015476ed
+K_symbolic = 1 / (1+exp(-_δ/_kT)) * (exp(-_ϵ△/_kT) + exp(-(_ϵ♡+_δ)/_kT))
+
+# ╔═╡ 2a47bede-fc8d-11ea-2234-517b57146be1
+n_symbolic = K_symbolic * _P / _kT / (1 + K_symbolic * _P / _kT)
+
+# ╔═╡ dc88b1ec-fc8d-11ea-0de4-f51f94533fea
+w_symbolic = exp(-_δ / _kT) / (1 + exp(-_δ / _kT)) * (1 + exp(-_ϵ♡ / _kT) * _P / _kT) / (1 + K_symbolic * _P / _kT)
+
+# ╔═╡ 45f2e688-fc8d-11ea-169a-fd11486da805
+begin
+	# too complicated to parse anyway
+	∂n_∂kT_symbolic = diff(n_symbolic, "kT")
+	∂K_∂kT_symbolic = diff(K_symbolic, "kT")
+end
+
+# ╔═╡ 6b68afd0-fc8d-11ea-30f2-cd181b6d5fcf
+md"
+to evaluate these symbolic expressions at numerical values
+"
+
+# ╔═╡ 5719a1d6-fc8d-11ea-000b-ddcb985f72f0
+begin
+	# if P is not involved
+	function eval_sym(sym::Sym, material::Material, kT::Float64)
+	    params = Dict("kT" => kT, "ϵ♡" => material.ϵ♡, "ϵ△" => material.ϵ△, "δ" => material.δ)
+	    return convert(Float64, sym.subs(params))
+	end
+	
+	# if P is involved
+	function eval_sym(sym::Sym, material::Material, kT::Float64, P::Float64)
+	    params = Dict("kT" => kT, "ϵ♡" => material.ϵ♡, "ϵ△" => material.ϵ△, "δ" => material.δ, "P" => P)
+	    return convert(Float64, sym.subs(params))
+	end
+	
+	function test_symbolic_expressions()
+		material = Material(rand(), -rand(), -rand())
+		kT = rand()
+		P = rand()
+
+		# K
+		@assert isapprox(K(material, kT), eval_sym(K_symbolic, material, kT))
+		# dK/d(kT)
+		@assert isapprox(∂K_∂kT(material, kT), eval_sym(∂K_∂kT_symbolic, material, kT))
+		# n (P=0.2)
+		@assert isapprox(n(material, kT, P), eval_sym(n_symbolic, material, kT, P))
+		# dn/d(kT)
+		@assert isapprox(∂n_∂kT(material, kT, P), eval_sym(∂n_∂kT_symbolic, material, kT, P))
+		
+		# w
+		@assert isapprox(w(material, kT, P), eval_sym(w_symbolic, material, kT, P))
+	end
+	
+	test_symbolic_expressions()
+end
+
+# ╔═╡ 4658aca4-fc8e-11ea-04b5-b78a558415a3
 
 
 # ╔═╡ Cell order:
@@ -257,4 +406,17 @@ end
 # ╠═73c59f08-fc89-11ea-153a-7987634c790f
 # ╟─4b8617e4-fc8a-11ea-0cd1-2ffba5c42303
 # ╠═517c173e-fc8a-11ea-28e6-ab0c43713125
-# ╠═c5d406d2-fc8a-11ea-378d-7388e9ad53d4
+# ╟─c5d406d2-fc8a-11ea-378d-7388e9ad53d4
+# ╠═dfdda3b0-fc8a-11ea-3777-c7fc194bd218
+# ╠═fa8835e0-fc8a-11ea-00cf-25aa4e5c3ffb
+# ╟─409daf48-fc8c-11ea-0811-d3bbaa86a15b
+# ╠═5b5c269a-fc8c-11ea-3eee-c3767d47d678
+# ╟─8f1e2f58-fc8c-11ea-1fde-9d1d72b5d631
+# ╠═9a054b92-fc8c-11ea-0aaf-f90ed61e9586
+# ╠═323ced64-fc8d-11ea-0ffe-7d5a015476ed
+# ╠═2a47bede-fc8d-11ea-2234-517b57146be1
+# ╠═dc88b1ec-fc8d-11ea-0de4-f51f94533fea
+# ╠═45f2e688-fc8d-11ea-169a-fd11486da805
+# ╟─6b68afd0-fc8d-11ea-30f2-cd181b6d5fcf
+# ╠═5719a1d6-fc8d-11ea-000b-ddcb985f72f0
+# ╠═4658aca4-fc8e-11ea-04b5-b78a558415a3
